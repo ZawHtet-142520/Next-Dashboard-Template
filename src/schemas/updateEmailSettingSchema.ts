9fb26d4156f7648c8cbf0f26a251c31d84904858
Toee Thiha Kyaw <toeethiha@cbs.com.mm>
2026-04-07 09:05:01 +0630

    chore(schema): add trim to string fields.

--- src/schemas/updateEmailSettingSchema.ts
+++ src/schemas/updateEmailSettingSchema.ts
@@ -1,11 +1,11 @@
 import { z } from "zod";
 
 export const updateEmailSettingSchema = z.object({
-  host: z.string().min(1, "Host is required"),
+  host: z.string().trim().min(1, "Host is required"),
   port: z.number().int().min(1).max(65535, "Invalid port number"),
   secure: z.boolean(),
   authUser: z.string().trim().email("Invalid email format for authUser"),
-  authPass: z.string().min(1, "Password is required"),
+  authPass: z.string().trim().min(1, "Password is required"),
   from: z.string().trim().min(1, "From is required"),
   email: z.string().toLowerCase().email("Invalid email format"),
 });
--- src/schemas/updateNotificationTemplateSchema.ts
+++ src/schemas/updateNotificationTemplateSchema.ts
@@ -4,9 +4,11 @@ export const updateNotificationTemplateSchema = z.object({
   _id: z.string(),
   subject: z
     .string({ required_error: "Subject is required" })
+    .trim()
     .min(1, "Subject is required"),
   template: z
     .string({ required_error: "Template is required" })
+    .trim()
     .min(1, "Template is required"),
   variables: z.array(z.string()),
 });
